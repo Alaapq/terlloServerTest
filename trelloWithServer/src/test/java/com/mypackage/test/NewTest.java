ebac945db8a19e7cfc2b50cfdd1048436b4d33b7
LENOVO <LENOVO@10.0.0.7>
2022-10-04 19:04:30 +0300

    commit nub 2

---
+++ trelloWithServer/src/test/java/com/mypackage/test/NewTest.java
@@ -0,0 +1,23 @@
+package com.mypackage.test;
+
+import java.awt.AWTException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() throws InterruptedException, AWTException {
+
+	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Driver//chromedriver.exe");
+	WebDriver driverchrome=new ChromeDriver();
+	driverchrome.get("https://demo.nopcommerce.com/register");
+	
+	//WebElement searchStore= driverchrome.findElement(By.xpath("//input[@id='small-searchterms']"));
+	
+	//System.out.println(searchStore.isDisplayed());
+	//System.out.println(searchStore.isEnabled());
+	
+  }
+}
--- trelloWithServer/src/test/java/com/mypackage/test/StatusOfWebElement.java
+++ trelloWithServer/src/test/java/com/mypackage/test/StatusOfWebElement.java
@@ -1,23 +1,26 @@
 package com.mypackage.test;
 
+import java.awt.AWTException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class StatusOfWebElement {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  @Test
+	  public void f() throws InterruptedException, AWTException {
 
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Driver//chromedriver.exe");
 		WebDriver driverchrome=new ChromeDriver();
 		driverchrome.get("https://demo.nopcommerce.com/register");
 		
-		WebElement searchStore= driverchrome.findElement(By.xpath("//input[@id='small-searchterms']"));
+		//WebElement searchStore= driverchrome.findElement(By.xpath("//input[@id='small-searchterms']"));
 		
-		System.out.println(searchStore.isDisplayed());
-		System.out.println(searchStore.isEnabled());
+		//System.out.println(searchStore.isDisplayed());
+		//System.out.println(searchStore.isEnabled());
 		
 		
 	}
