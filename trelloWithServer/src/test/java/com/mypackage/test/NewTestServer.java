bd4506bd77b66e001a828b8ef4f7ceb6dfe26b1e
LENOVO <LENOVO@10.0.0.7>
2022-10-04 19:08:33 +0300

    commite numb 4

--- trelloWithServer/src/test/java/com/mypackage/test/NewTestServer.java
+++ trelloWithServer/src/test/java/com/mypackage/test/NewTestServer.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class NewTest {
+public class NewTestServer {
   @Test
   public void f() throws InterruptedException, AWTException {
 
