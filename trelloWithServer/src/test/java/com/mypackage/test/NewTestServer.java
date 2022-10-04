3af6f2cd1e46b2d9fa28b9f1de8899f5eb2ae604
LENOVO <LENOVO@10.0.0.7>
2022-10-04 19:09:59 +0300

    commite numb 5

--- trelloWithServer/src/test/java/com/mypackage/test/NewTestServer.java
+++ trelloWithServer/src/test/java/com/mypackage/test/NewTestServer.java
@@ -1,9 +1,12 @@
 package com.mypackage.test;
 
 import java.awt.AWTException;
+import java.io.File;
+import java.util.HashMap;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class NewTestServer {
@@ -11,7 +14,17 @@ public class NewTestServer {
   public void f() throws InterruptedException, AWTException {
 
 	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Driver//chromedriver.exe");
-	WebDriver driverchrome=new ChromeDriver();
+	  String downloadFilepath="downloads";
+	  File file =new File(downloadFilepath);
+	 HashMap<String, Object> chromeprefs = new HashMap<String, Object>();
+	  chromeprefs.put("profile.default_content_settings.popups", 0);
+	   chromeprefs.put("download.default_directory", file.getAbsolutePath());
+	    ChromeOptions options = new ChromeOptions();
+	     options.setExperimentalOption("prefs", chromeprefs);
+	      options.addArguments("--start-maximied");
+	       options.addArguments("--headless");
+	        options.addArguments("--disable-infobars");
+	 	 	WebDriver driverchrome=new ChromeDriver(options);
 	driverchrome.get("https://demo.nopcommerce.com/register");
 	
 	//WebElement searchStore= driverchrome.findElement(By.xpath("//input[@id='small-searchterms']"));
